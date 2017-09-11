b03bbc6d64b22cc9780b49854054edeb5239724f
zt19994 <zt191610942@163.com>
2017-09-11 19:32:23 +0800

    add a article

--- blog/admin.py
+++ blog/admin.py
@@ -5,6 +5,12 @@ from django.contrib import admin
 from .models import Category, Tag, Post
 
 # Register your models here.
+
+
+class PostAdmin(admin.ModelAdmin):
+    """ 可以在后台Post中显示指定信息"""
+    list_display = ['title', 'created_time', 'modified_time', 'category', 'author']
+
 admin.site.register(Category)
 admin.site.register(Tag)
-admin.site.register(Post)
+admin.site.register(Post, PostAdmin)
